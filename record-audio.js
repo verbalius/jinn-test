70d1b0c541640e74a3659f2e6de2e6e36a8c8ab1
Dymytrii Ishchuk <39443782+b4rracuda@users.noreply.github.com>
2020-01-21 19:23:08 +0200

    JS update
    - Fixed code failure when using non-HTTPS connection
    - Added jQuery and its validation to run .js properly
    - Added sending recorded audio to server as base64, on response we make an API call with an example audio link (should be a link to the recorded file on server)

--- record-audio.js
+++ record-audio.js
@@ -12,44 +12,76 @@
 
 var recordButton, stopButton, recorder;
 
-window.onload = function () {
-  recordButton = document.getElementById('record');
-  stopButton = document.getElementById('stop');
+// Check if jQuery is loaded
+if (typeof(jQuery)==='function'){
+  jQuery(function($){
+    recordButton = document.getElementById('record');
+    stopButton = document.getElementById('stop');
+    // First we check if the connection is secure (if not, typeof mediaDevices will be undefined)
+    if (navigator.mediaDevices!==undefined) {
+      // get audio stream from user's mic
+      navigator.mediaDevices.getUserMedia({
+        audio: true
+      })
+      .then(function (stream) {
+        recordButton.disabled = false;
+        recordButton.addEventListener('click', startRecording);
+        stopButton.addEventListener('click', stopRecording);
+        var options;
+        recorder = new MediaRecorder(stream);
+        // listen to dataavailable, which gets triggered whenever we have
+        // an audio blob available
+        recorder.addEventListener('dataavailable', onRecordingReady);
+      })
+      .catch(function (err){
+        console.log(err);
+      });
+      
+    };
 
-  // get audio stream from user's mic
-  navigator.mediaDevices.getUserMedia({
-    audio: true
-  })
-  .then(function (stream) {
-    recordButton.disabled = false;
-    recordButton.addEventListener('click', startRecording);
-    stopButton.addEventListener('click', stopRecording);
-    var options;
-    recorder = new MediaRecorder(stream);
-    // listen to dataavailable, which gets triggered whenever we have
-    // an audio blob available
-    recorder.addEventListener('dataavailable', onRecordingReady);
+    function startRecording() {
+      recordButton.disabled = true;
+      stopButton.disabled = false;
+
+      recorder.start();
+    }
+
+    function stopRecording() {
+      recordButton.disabled = false;
+      stopButton.disabled = true;
+
+      // Stopping the recorder will eventually trigger the `dataavailable` event and we can complete the recording process
+      recorder.stop();
+    }
+
+    function onRecordingReady(e) {
+      var audio = document.getElementById('audio');
+      // e.data contains a blob representing the recording
+      console.log(e.data);
+      var audioBlobSrc = window.URL.createObjectURL(e.data);
+      audio.src = audioBlobSrc;
+      audio.play();
+      // Convert blob to base64, send it to server to save, on response - make a request to Audd.io API
+      var reader = new FileReader();
+      reader.readAsDataURL(e.data); 
+      reader.onloadend = function() {
+        var base64data = reader.result;    
+        $.post("audioparser.php", {"audio" : base64data}, function(){
+          let auddata = {
+            'url': 'https://host.ishchukdev.com/jinn/upload/1579626267.mp3',
+            'return': 'timecode,apple_music,deezer,spotify',
+            'api_token': '8fb2705870b9e8291b72fbbe6b679a06'
+          }
+          $.getJSON('https://api.audd.io/?jsonp=?', auddata, function(result){
+              console.log(result);
+          });
+        });
+      }
+      
+    }
+
   });
-};
-
-function startRecording() {
-  recordButton.disabled = true;
-  stopButton.disabled = false;
-
-  recorder.start();
 }
-
-function stopRecording() {
-  recordButton.disabled = false;
-  stopButton.disabled = true;
-
-  // Stopping the recorder will eventually trigger the `dataavailable` event and we can complete the recording process
-  recorder.stop();
-}
-
-function onRecordingReady(e) {
-  var audio = document.getElementById('audio');
-  // e.data contains a blob representing the recording
-  audio.src = URL.createObjectURL(e.data);
-  audio.play();
+else{
+  console.log('Error! Jquery is not loaded')
 }
