e054c2fbce6b52979fd7701118713c7dd0fcb3f4
Dymytrii Ishchuk <39443782+b4rracuda@users.noreply.github.com>
2020-01-24 23:10:33 +0200

    Front-end updates. 'Back' Button. Material icon font
    Fixed Divider vectors for step1/2
    added cross-browser vendor prefixes

--- static/scripts.js
+++ static/scripts.js
@@ -2,6 +2,7 @@ jQuery(function($){
   $('#tostep2').click(()=>{toStep(2);});
   $('#tostep31').click(()=>{toStep(31);});
   $('#tostep32').click(()=>{toStep(32);});
+  $('button#reset').click(()=>{$('button#reset').hide();});
 });
 function toStep(num){
   let selector = '#Step'+num;
@@ -9,6 +10,9 @@ function toStep(num){
     $('.jinn-step.active').removeClass('active');
     $(selector).fadeIn(200, ()=>{
       $(selector).addClass('active');
+      if ($('.jinn-step.active#Step1').length===0) {
+        $('button#reset').show();
+      }
     });
   });  
 }
