d7593f74f9e85b3ef96b2bd94517689b3cd0d8c5
volodymyrleha <33492547+volodymyrleha@users.noreply.github.com>
2020-01-25 06:13:07 +0200

    better wave + mobile step31

--- static/scripts.js
+++ static/scripts.js
@@ -17,17 +17,20 @@ function toStep(num){
   });  
 }
 
+  
 
 var c = document.getElementById("myCanvas");
 
 var check_if_done = false;
 
 c.width = window.innerWidth;
-c.height = window.innerHeight / 100 * 60;
+c.height = window.innerHeight / 100 * 50;
+
+var lines = 7;
 
 var width = c.width;
 var height = c.height;
-var step = width / 7;
+var step = width / lines;
 
 var ctx = c.getContext("2d");
 ctx.moveTo(0, height / 2);
@@ -43,79 +46,133 @@ ctx.strokeStyle = gradient;
 ctx.lineWidth = 3;
 ctx.stroke();
 
-
 function sleep(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
+  return new Promise(resolve => setTimeout(resolve, ms));
 }
 
+var pre_top_center = height / 2;
+var pre_top_corner = 0;
 
-var previous_heights = [c.height, c.height, c.height, c.height, c.height, c.height, c.height, c.height];
-var duration = 100;
-var new_heights = [0, 0, 0, 0, 0, 0, 0, 0]
+async function draw_waves(duration) {
+  pre_top_center = height / 2;
+  pre_top_corner = 0;
 
-new_heights[0] = Math.floor(Math.random() * 180) + 3;
-new_heights[1] = Math.floor(Math.random() * (220 - 100)) + 100;
-new_heights[2] = Math.floor(Math.random() * 180) + 3;
-new_heights[3] = Math.floor(Math.random() * (220 - 100)) + 100;
-new_heights[4] = Math.floor(Math.random() * 180) + 3;
-new_heights[5] = Math.floor(Math.random() * (220 - 100)) + 100;
-new_heights[6] = Math.floor(Math.random() * 180) + 3;
-new_heights[7] = Math.floor(Math.random() * (220 - 100)) + 100;
+  for (var j = 0; j < duration; j++) {
+    for (var k = 0; k < 2; k++) {
+        var mirror_coff = 1;
+        var mirror_height = 0;
+
+        if (k == 1) {
+          mirror_coff = -1;
+          mirror_height = c.height;
+        }
+
+      for (var i = 0; i < 2; i++) {
+        if (i == 1) {
+          ctx.setTransform(-1, 0, 0, mirror_coff, c.width, mirror_height);
+        } else {
+          ctx.setTransform(1, 0, 0, mirror_coff, 0, mirror_height);
+        }
+
+        ctx.beginPath();
+        ctx.moveTo(0, height / 2);
 
 
-console.log(c.height);
+        ctx.bezierCurveTo(step, height / 2,
+                          step, height / 2 + (height / 4 - height / 2) / duration * j,
+                          step * 2, height / 2 + (height / 8) / duration * j);
 
-async function anim(duration) {
-  while (!check_if_done) {
-    for(var j = 0; j < duration; j++) {
-      ctx.clearRect(0, 0, c.width, c.height);
-      ctx.beginPath();
-      ctx.moveTo(0, height / 2);
 
-      var height_temp = 0;
+        ctx.bezierCurveTo(step * 3, height / 2 + (height - height / 2) / duration * j,
+                          step * 3, height / 2,
+                          step * 4 - step / 2, height / 2);
 
-      for (var i = 0; i < 8; i = i + 2) {
-        var left_p = height_temp / height / 2 * step; 
-        var height_q = previous_heights[i] + (new_heights[i] - previous_heights[i]) / duration * j;
-        var right_q = height_q / height / 2 * step;
-        height_temp = height_q;
+        var gradient = ctx.createLinearGradient(0, 0, c.width, 0);
+        if (mirror_coff == 1) {
+          gradient.addColorStop("0", "#D22280");
+          gradient.addColorStop("0.5" ,"#621AD3");
+          gradient.addColorStop("1.0", "#E52372");
+        } else {
+          gradient.addColorStop("0", "#621AD3");
+          gradient.addColorStop("0.5" ,"#D9227B");
+          gradient.addColorStop("1.0", "#450F93");
+        }
 
-        var height_b = previous_heights[i + 1] + (new_heights[i + 1] - previous_heights[i + 1]) / duration * j;
-        var height_p = (1 - height_b / height) * step - (1 - height_b / height) * 100;
-        
-            
-        ctx.bezierCurveTo(step * i + height_p + left_p, height * 1.5 - height_b, 
-                  step * (i + 1) - height_p - right_q, height * 1.5 - height_b,
-                  step * (i + 1), height / 2);
+        ctx.strokeStyle = gradient;
 
-        ctx.quadraticCurveTo(step * (i + 2) - step / 2, - height / 2 + height_q, step * (i + 2), height / 2);
+        ctx.lineWidth = 3;
+        ctx.stroke();
       }
 
-    
+    }
+
+    await sleep(1);
+    ctx.clearRect(0, 0, c.width, c.height);
+  }
+
+  while (!check_if_done) {
+    var top_center = Math.floor(Math.random() * (height / 2 - 3) + 3);
+    var top_corner = Math.floor(Math.random() * (height / 2));
+
+    for (var j = 0; j < duration; j++) {
+      coff_corner = (top_corner - pre_top_corner) / duration * j;
+      coff_center = (top_center - pre_top_center) / duration * j;
+
+      for (var k = 0; k < 2; k++) {
+        var mirror_coff = 1;
+        var mirror_height = 0;
+
+        if (k == 1) {
+          mirror_coff = -1;
+          mirror_height = c.height;
+        }
+
+        for (var i = 0; i < 2; i++) {
+          if (i == 1) {
+            ctx.setTransform(-1, 0, 0, mirror_coff, c.width, mirror_height);
+          } else {
+            ctx.setTransform(1, 0, 0, mirror_coff, 0, mirror_height);
+          }
+
+          ctx.beginPath();
+          ctx.moveTo(0, height / 2);
 
 
-      var gradient = ctx.createLinearGradient(0, 0, c.width, 0);
-      gradient.addColorStop("0", "#D22280");
-      gradient.addColorStop("0.5" ,"#621AD3");
-      gradient.addColorStop("1.0", "#E52372");
+          ctx.bezierCurveTo(step, height / 2,
+                            step, height / 4 - (pre_top_corner + coff_corner),
+                            step * 2, height / 2 + height / 8 - (pre_top_corner + coff_corner) / 2);
 
-      ctx.strokeStyle = gradient;
 
-      ctx.lineWidth = 3;
-      ctx.stroke();
+          ctx.bezierCurveTo(step * 3, height,
+                            step * 3, pre_top_center + coff_center,
+                            step * 4 - step / 2, pre_top_center + coff_center);
+
+          var gradient = ctx.createLinearGradient(0, 0, c.width, 0);
+
+          if (mirror_coff == 1) {
+            gradient.addColorStop("0", "#D22280");
+            gradient.addColorStop("0.5" ,"#621AD3");
+            gradient.addColorStop("1.0", "#E52372");
+          } else {
+            gradient.addColorStop("0", "#621AD3");
+            gradient.addColorStop("0.5" ,"#D9227B");
+            gradient.addColorStop("1.0", "#450F93");
+          }
+
+          ctx.strokeStyle = gradient;
+
+          ctx.lineWidth = 3;
+          ctx.stroke();
+        }
+      }
+
 
       await sleep(1);
+      ctx.clearRect(0, 0, c.width, c.height);
     }
 
-    for (var i = 0; i < 8; i++) {
-      previous_heights[i] = new_heights[i];
-
-      if (i % 2 == 0) {
-        new_heights[i] = Math.floor(Math.random() * (c.height - 100 - 10)) + 10;
-      } else {
-        new_heights[i] = Math.floor(Math.random() * (c.height - 80 - 200)) + 200;
-      }
-    }
+    pre_top_corner = top_corner;
+    pre_top_center = top_center;
   }
 }
 
@@ -147,28 +204,29 @@ $(document).ready(function(){
   $("#replay").hide();
   $("#done").hide();
 
-    $("#record").click(function() {
-      $(this).hide();
-      $("#replay").hide();
+  $("#record").click(function() {
+    $(this).hide();
+    $("#replay").hide();
     $("#done").hide();
-      $("#stop").show();
-      anim(60);
+    $("#stop").show();
 
-      $(".status").find("p").text("Слухаю...");
+    draw_waves(60);
 
-      run_timer();
-    });
+    $(".status").find("p").text("Слухаю...");
 
-    $("#stop").click(function() {
-      $(this).hide();
-      $(".status").find("p").text("Готово!");
-      $("#replay").show();
-      $("#done").show();
-      check_if_done = true;
-    });
+    run_timer();
+  });
 
-    $("#replay").click(function() {
-      check_if_done = false;
-      $("#record").click();
-    });
+  $("#stop").click(function() {
+    $(this).hide();
+    $(".status").find("p").text("Готово!");
+    $("#replay").show();
+    $("#done").show();
+    check_if_done = true;
+  });
+
+  $("#replay").click(function() {
+    check_if_done = false;
+    $("#record").click();
+  });
 });
